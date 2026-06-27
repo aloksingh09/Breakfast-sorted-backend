dd2b8a581a2f02b7f893efb2c8ba175a19063706
Shreyanshi200 <0shrivastavashreyanshi@gmail.com>
2026-06-28 00:01:50 +0530

    Fix CORS headers configuration rules and auto-table definitions metrics

--- core/settings.py
+++ core/settings.py
@@ -79,6 +79,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
